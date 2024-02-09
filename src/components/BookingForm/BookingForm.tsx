18bc14cdf7ee5a8dc531561c57b0dcddd7b1bb40
Marco Rosner <marco.rosner@gmail.com>
2024-02-09 11:41:37 -0300

    refact: responsiviness and add new components

--- src/components/BookingForm/BookingForm.tsx
+++ src/components/BookingForm/BookingForm.tsx
@@ -1,68 +1,45 @@
-import { Autocomplete, Button, Grid, TextField } from "@mui/material";
-import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import dayjs from "dayjs";
-import 'dayjs/locale/en'
-import utc from "dayjs/plugin/utc";
-import timezone from "dayjs/plugin/timezone";
-dayjs.extend(utc);
-dayjs.extend(timezone);
-
 import React from "react";
+import { Autocomplete, Button, Grid, TextField } from "@mui/material";
+
 import { places } from "../../mock/places";
+import { DatePickers } from "../DatePickers/DatePickers";
 
 export const BookingForm = (): React.ReactElement => {
     return (
         <Grid
             container
+            item
             direction="row"
             justifyContent="center"
             alignItems="center"
-            spacing={4}
-            sx={{ minWidth: '200px' }}>
-                <Grid item>
-                    <Autocomplete
-                        freeSolo
-                        id="searchPlace"
-                        disableClearable
-                        options={places.map((place) => place.name)}
-                        renderInput={(params) => (
-                            <TextField
-                                {...params}
-                                label="Search Place"
-                                sx={{ minWidth: '200px' }}
-                                InputProps={{
-                                    ...params.InputProps,
-                                    type: 'search'
-                                }}
-                            />
-                        )}
-                    />
-                </Grid>
-
-                <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={'en'}>
-                    <Grid item>
-                        <DatePicker
-                            label="Start Date"
-                            sx={{ minWidth: '200px' }}
-                            onChange={(newValue) => {
-                                console.log(newValue);
+            spacing={3}
+            sx={{ minWidth: '200px', marginTop: '10px' }}
+            xs={4}
+            sm={12}
+        >
+            <Grid item>
+                <Autocomplete
+                    freeSolo
+                    id="searchPlace"
+                    disableClearable
+                    options={places.map((place) => place.name)}
+                    renderInput={(params) => (
+                        <TextField
+                            {...params}
+                            label="Search Place"
+                            sx={{ minWidth: '300px' }}
+                            InputProps={{
+                                ...params.InputProps,
+                                type: 'search'
                             }}
                         />
-                    </Grid>
-                    <Grid item>
-                        <DatePicker
-                            label="End Date"
-                            sx={{ minWidth: '200px' }}
-                            onChange={(newValue) => {
-                                console.log(newValue);
-                            }}
-                        />
-                    </Grid>
-                </LocalizationProvider>
-                <Grid item>
-                    <Button>Search</Button>
-                </Grid>
+                    )}
+                />
+            </Grid>
+            <DatePickers />
+            <Grid item>
+                <Button size="large" sx={{ color: 'white' }}>Search</Button>
+            </Grid>
         </Grid>
     )
 }
