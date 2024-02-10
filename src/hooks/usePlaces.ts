7fa4d9b0bc36764adbfac7ae3d20db0f68cb84d6
Marco Rosner <marco.rosner@gmail.com>
2024-02-09 22:05:07 -0300

    feat: Add usePlaces hook

---
+++ src/hooks/usePlaces.ts
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { Place, places } from "../mock/places";
+
+interface usePlacesInterface {
+    loading: boolean;
+    error: boolean;
+    data: Place[]
+}
+
+export const usePlaces = (): usePlacesInterface => {
+    const [loading, setLoading] = useState<boolean>(true)
+    const [error, setError] = useState<boolean>(false)
+    const [data, setData] = useState<Place[]>([])
+
+    // Mocking backend request
+    const min = 1000
+    const max = 3000
+    const timeout = Math.random() * (min - max) + min
+    const backendService = new Promise((resolve) => setTimeout(() => resolve(places), timeout));
+
+    backendService.then(() => {
+        setLoading(false)
+        setError(false)
+        setData(places)
+    }).catch(() => {
+        setLoading(false)
+        setError(true)
+    })
+
+    return { loading, error, data }
+}
