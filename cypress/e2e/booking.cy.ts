a776ec6aca3d3832c12fd3e90a0395f4a37bc169
Marco Rosner <marco.rosner@gmail.com>
2024-02-20 09:37:45 -0300

    test: Add default/mocked ids to E2E tests

--- cypress/e2e/booking.cy.ts
+++ cypress/e2e/booking.cy.ts
@@ -1,5 +1,8 @@
 describe("Booking", () => {
     it("should create a booking", () => {
+        const propertyId = 118729
+        const bookingId = 123842
+
         cy.visit('http://localhost:5173')
         cy.get('#searchPlace').click().type('Maceió')
         cy.get('#searchPlace-option-0').click()
@@ -11,24 +14,29 @@ describe("Booking", () => {
 
         cy.wait(3000)
 
-        cy.get('[data-cy="reserve-118729"]').should("exist")
-        cy.get('[data-cy="reserve-118729"]').click()
+        cy.get(`[data-cy="reserve-${propertyId}"]`).should("exist")
+        cy.get(`[data-cy="reserve-${propertyId}"]`).click()
 
         cy.wait(5000)
 
         // check manage view
-        cy.get('[data-cy="update-booking-123842"]').should("exist")
-        cy.get('[data-cy="delete-booking-123842"]').should("exist")
+        cy.get(`[data-cy="update-booking-${bookingId}"]`).should("exist")
+        cy.get(`[data-cy="delete-booking-${bookingId}"]`).should("exist")
     })
 
     it("should access manage booking view", () => {
-        // accessing manage booking view
+        const propertyId = 118729
+        const bookingId = 123842
+
         cy.visit('http://localhost:5173')
-        
+
+        // accessing manage booking view
         cy.get('[data-cy="user-menu"]').should("exist")
         cy.get('[data-cy="user-menu"]').click()
         cy.get('[data-cy="manage-user-menu"]').should("exist")
         cy.get('[data-cy="manage-user-menu"]').click()
+        cy.contains("No Bookings").should('exist')
+        
         cy.wait(3000)
 
         // booking
@@ -41,20 +49,24 @@ describe("Booking", () => {
         cy.get('[data-cy="submit"]').click()
 
         cy.wait(3000)
-        
-        cy.get('[data-cy="reserve-118729"]').should("exist")
-        cy.get('[data-cy="reserve-118729"]').click()
-        
+
+        cy.get(`[data-cy="reserve-${propertyId}"]`).should("exist")
+        cy.get(`[data-cy="reserve-${propertyId}"]`).click()
+
         cy.wait(5000)
-        
+
         // check manage view
-        cy.get('[data-cy="update-booking-123842"]').should("exist")
-        cy.get('[data-cy="delete-booking-123842"]').should("exist")
+        cy.get(`[data-cy="update-booking-${bookingId}"]`).should("exist")
+        cy.get(`[data-cy="delete-booking-${bookingId}"]`).should("exist")
     })
 
     it('should update a booking', () => {
-        // booking
+        const propertyId = 118729
+        const bookingId = 123842
+
         cy.visit('http://localhost:5173')
+
+        // booking
         cy.get('[data-cy="submit"]').should('be.disabled')
         cy.get('#searchPlace').click().type('Maceió')
         cy.get('#searchPlace-option-0').click()
@@ -64,35 +76,38 @@ describe("Booking", () => {
         cy.get('[data-cy="submit"]').click()
 
         cy.wait(3000)
-        
-        cy.get('[data-cy="reserve-118729"]').should("exist")
-        cy.get('[data-cy="reserve-118729"]').click()
-        
+
+        cy.get(`[data-cy="reserve-${propertyId}"]`).should("exist")
+        cy.get(`[data-cy="reserve-${propertyId}"]`).click()
+
         cy.wait(5000)
-        
+
         // check manage view
-        cy.get('[data-cy="update-booking-123842"]').should("exist")
-        cy.get('[data-cy="delete-booking-123842"]').should("exist")
+        cy.get(`[data-cy="update-booking-${bookingId}"]`).should("exist")
+        cy.get(`[data-cy="delete-booking-${bookingId}"]`).should("exist")
 
         // updating
-        cy.get('[data-cy="delete-booking-123842"]').should("exist")
-        cy.get('[data-cy="update-booking-123842"]').click()
+        cy.get(`[data-cy="update-booking-${bookingId}"]`).click()
         cy.get('#update-start-date').type('03/07/2025')
         cy.get('#update-end-date').type('03/10/2025')
 
         cy.get('[data-cy="update-button"]').should("exist")
         cy.get('[data-cy="update-button"]').click()
-        
+
         cy.wait(5000)
 
         // check manage view
-        cy.get('[data-cy="update-booking-123842"]').should("exist")
-        cy.get('[data-cy="delete-booking-123842"]').should("exist")
+        cy.get(`[data-cy="update-booking-${bookingId}"]`).should("exist")
+        cy.get(`[data-cy="delete-booking-${bookingId}"]`).should("exist")
     })
 
     it('should delete a booking', () => {
-        // booking
+        const propertyId = 118729
+        const bookingId = 123842
+
         cy.visit('http://localhost:5173')
+
+        // booking
         cy.get('[data-cy="submit"]').should('be.disabled')
         cy.get('#searchPlace').click().type('Maceió')
         cy.get('#searchPlace-option-0').click()
@@ -102,21 +117,21 @@ describe("Booking", () => {
         cy.get('[data-cy="submit"]').click()
 
         cy.wait(3000)
-        
-        cy.get('[data-cy="reserve-118729"]').should("exist")
-        cy.get('[data-cy="reserve-118729"]').click()
-        
+
+        cy.get(`[data-cy="reserve-${propertyId}"]`).should("exist")
+        cy.get(`[data-cy="reserve-${propertyId}"]`).click()
+
         cy.wait(5000)
-        
+
         // check manage view
-        cy.get('[data-cy="update-booking-123842"]').should("exist")
-        cy.get('[data-cy="delete-booking-123842"]').should("exist")
+        cy.get(`[data-cy="update-booking-${bookingId}"]`).should("exist")
+        cy.get(`[data-cy="delete-booking-${bookingId}"]`).should("exist")
 
         // deleting
-        cy.get('[data-cy="delete-booking-123842"]').click()
-        
+        cy.get(`[data-cy="delete-booking-${bookingId}"]`).click()
+
         cy.wait(1000)
-        
+
         cy.contains("No Bookings").should('exist')
     })
 })
