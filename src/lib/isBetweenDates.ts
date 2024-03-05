6f438a90170d167522861d87ce450cee6390a991
Marco Rosner <marco.rosner@gmail.com>
2024-03-05 10:47:48 -0300

    fix: allow same end day booking

--- src/lib/isBetweenDates.ts
+++ src/lib/isBetweenDates.ts
@@ -5,8 +5,8 @@ import { StartEndDates } from "../types"
 dayjs.extend(isBetween)
 
 export const isBetweenDates = (datesToCheck: StartEndDates, datesToValidate: StartEndDates) => {
-    const sDate = dayjs(datesToCheck.startDate).isBetween(datesToValidate.startDate, datesToValidate.endDate, "days", "[]")
-    const eDate = dayjs(datesToCheck.endDate).isBetween(datesToValidate.startDate, datesToValidate.endDate, "days", "[]")
+    const sDate = dayjs(datesToCheck.startDate).isBetween(datesToValidate.startDate, datesToValidate.endDate, "days", "(]")
+    const eDate = dayjs(datesToCheck.endDate).isBetween(datesToValidate.startDate, datesToValidate.endDate, "days", "(]")
 
     return sDate || eDate
 }
